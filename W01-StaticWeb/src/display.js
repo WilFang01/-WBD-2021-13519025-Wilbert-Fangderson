2c0ec4374789dfcabd7696e90a55c9763484e688
Wilbert Fangderson <wilbertf38@gmail.com>
2021-08-29 21:01:23 +0700

    Yay

--- W01-StaticWeb/src/display.js
+++ W01-StaticWeb/src/display.js
@@ -1,85 +1,46 @@
-var data = {
-    "books": [
-      {
-        "judul": "Untuk Apa Seni",
-        "primer": {
-          "url-foto": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1392706216l/20837627.jpg",
-          "harga": 60000
-        },
-        "deskripsi": {
-          "penulis": "Bambang Sugiharto, dkk",
-          "penerbit": "Pustaka Matahari",
-          "penyunting": "Bambang Sugiharto"
-        }
-      },
-      {
-        "judul": "Warisan Sejarah Arianisme",
-        "primer": {
-          "url-foto": "https://pustaka.iainbukittinggi.ac.id/wp-content/uploads/2018/12/arian-198x300.jpg",
-          "harga": 97000
-        },
-        "deskripsi": {
-          "judul-asli": "Archetypal Heresy: Arianism Through the Centuries",
-          "penulis": "Maurice Wiles",
-          "penerjemah": "Zaenal Muttaqin",
-          "penerbit": "Pustaka Matahari",
-          "penerbit-asli": "Oxford University Press, Inc."
-        }
-      },
-      {
-        "judul": "Sejarah Filsafat Kontemporer: Jerman dan Inggris",
-        "primer": {
-          "url-foto": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1551165807l/4309628._SX318_.jpg",
-          "harga": 70000
-        },
-        "deskripsi": {
-          "penulis": "K. Bertens",
-          "penerbit": "PT Gramedia Pustaka Utama"
-        }
-      },
-      {
-        "judul": "Sejarah Filsafat Kontemporer: Prancis",
-        "primer": {
-          "url-foto": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1243418656l/6498943.jpg",
-          "harga": 63000
-        },
-        "deskripsi": {
-          "penulis": "K. Bertens",
-          "penerbit": "PT Gramedia Pustaka Utama"
-        }
-      },
-      {
-        "judul": "Semiotika dan Hipersemiotika",
-        "primer": {
-          "url-foto": "https://s2.bukalapak.com/img/7734600261/large/IMG_20170912_134621_scaled.jpg",
-          "harga": 120000
-        },
-        "deskripsi": {
-          "penulis": "Yasraf Amir Piliang",
-          "penerbit": "Pustaka Matahari"
-        }
-      },
-      {
-        "judul": "Epistemologi Dasar",
-        "primer": {
-          "url-foto": "https://togamas.com/css/images/items/potrait/JPEGG_5905_Epistemologi_Dasar.jpg",
-          "harga": 60000
-        },
-        "deskripsi": {
-          "penulis": "J. Sudarminta",
-          "penerbit": "Penerbit Kanisius"
-        }
-      },
-      {
-        "judul": "Teori-Teori Etika",
-        "primer": {
-          "url-foto": "https://s2.bukalapak.com/img/2027491742/large/Buku_Teori_Teori_Etika_karya_Gordon_Graham.jpg",
-          "harga": 96000
-        },
-        "deskripsi": {
-          "penulis": "Nusamedia",
-          "penerbit": "Gordon Graham"
-        }
-      }
-    ]
-  }
+const content = document.getElementById("content");
+
+const searchParam = window.location.search;
+const urlParam = new URLSearchParams(searchParam);
+
+const book = data.books[urlParam.get("index")]
+
+function showBook(book) {
+    const myArticle = document.createElement('article');
+    const penulis = document.createElement('p');
+    const penerbit = document.createElement('p');
+    const penyunting = document.createElement('p');
+    const penerjemah = document.createElement('p');
+    const judulasli = document.createElement('p');
+    const penerbitasli = document.createElement('p');
+
+
+    if (book.deskripsi.penulis){
+        penulis.textContent= 'Penulis : ' + book.deskripsi.penulis;
+        myArticle.appendChild(penulis);
+    }
+    if (book.deskripsi.penerbit){
+        penerbit.textContent= 'Penerbit : ' + book.deskripsi.penerbit;
+        myArticle.appendChild(penerbit);
+    }
+    if (book.deskripsi.penyunting){
+        penyunting.textContent= 'Penyunting : ' + book.deskripsi.penyunting;
+        myArticle.appendChild(penyunting);
+    }
+    if (book.deskripsi.penerjemah){
+        penerjemah.textContent= 'Penerjemah : ' + book.deskripsi.penerjemah;
+        myArticle.appendChild(penerjemah);
+    }
+    if (book.deskripsi["judul-asli"]){
+        judulasli.textContent= 'Judul asli : ' + book.deskripsi["judul-asli"];
+        myArticle.appendChild(judulasli);
+    }
+    if (book.deskripsi["penerbit-asli"]){
+        penerbitasli.textContent= 'Penerbit asli : ' + book.deskripsi["penerbit-asli"];
+        myArticle.appendChild(penerbitasli);
+    }
+
+    content.appendChild(myArticle);
+}
+
+showBook(book);
